6f4a5cc04b90a8562b4ccd99b782dc96d0095604
MoltenEcdysone <pradyumna17@iiserb.ac.in>
2022-09-15 13:10:41 +0530

    The first push!

---
+++ themes/nord.h
@@ -0,0 +1,60 @@
+// #define black   #181926
+// #define pn1     #2e3440
+// #define pn2     #3b4252
+// #define pn3     #434c5e
+// #define pn4     #4c566a
+// #define ss1     #d8dee9
+// #define ss2     #e5e9f0
+// #define ss3     #eceff4
+// #define frost1  #8fbcbb
+// #define frost2  #88c0d0
+// #define frost3  #81a1c1
+// #define frost4  #5e81ac
+// #define aurora1 #bf616a
+// #define aurora2 #d08770
+// #define aurora3 #ebcb8b
+// #define aurora4 #a3be8c
+// #define aurora5 #b48ead
+
+
+static char c000000[]                    = "#bf616A"; // placeholder value
+
+static char normfgcolor[]                = "#d08770";
+static char normbgcolor[]                = "#151a22";
+static char normbordercolor[]            = "#151a22";
+static char normfloatcolor[]             = "#4c566a";
+
+static char selfgcolor[]                 = "#5e81ac";
+static char selbgcolor[]                 = "#151a22";
+static char selbordercolor[]             = "#d08770";
+static char selfloatcolor[]              = "#ebcb8b";
+
+static char titlenormfgcolor[]           = "#4c566a";
+static char titlenormbgcolor[]           = "#151a22";
+static char titlenormbordercolor[]       = "#151a22";
+static char titlenormfloatcolor[]        = "#4c566a";
+
+static char titleselfgcolor[]            = "#151a22";
+static char titleselbgcolor[]            = "#bf616a";
+static char titleselbordercolor[]        = "#ebcb8b";
+static char titleselfloatcolor[]         = "#a3be8c";
+
+static char tagsnormfgcolor[]            = "#cdd2dd";
+static char tagsnormbgcolor[]            = "#151a22";
+static char tagsnormbordercolor[]        = "#cdd2dd";
+static char tagsnormfloatcolor[]         = "#151a22";
+
+static char tagsselfgcolor[]             = "#151a22";
+static char tagsselbgcolor[]             = "#bf616a";
+static char tagsselbordercolor[]         = "#151a22";
+static char tagsselfloatcolor[]          = "#bf616a";
+
+static char hidnormfgcolor[]             = "#b48ead";
+static char hidselfgcolor[]              = "#b48ead";
+static char hidnormbgcolor[]             = "#151a22";
+static char hidselbgcolor[]              = "#151a22";
+
+static char urgfgcolor[]                 = "#151a22";
+static char urgbgcolor[]                 = "#bf616a";
+static char urgbordercolor[]             = "#ebcb8b";
+static char urgfloatcolor[]              = "#a3be8c";
